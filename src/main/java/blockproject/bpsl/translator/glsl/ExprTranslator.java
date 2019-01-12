91a94efc39ae0f5c88fa73aa2ea9927457958db6
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-01-12 19:54:54 +0100

    Advanced GLSL ExprTranslator

--- src/main/java/blockproject/bpsl/translator/glsl/ExprTranslator.java
+++ src/main/java/blockproject/bpsl/translator/glsl/ExprTranslator.java
@@ -1,6 +1,31 @@
 package blockproject.bpsl.translator.glsl;
 
-public class ExprTranslator
+import java.util.HashMap;
+import java.util.Map;
+
+import blockproject.bpsl.ast.expr.Expr;
+
+public abstract class ExprTranslator
 {
-    
+    private static Map<Expr.EType, ExprTranslator> translators = new HashMap<>();
+
+    public abstract Expr.EType type();
+    public abstract String translate(Expr expr);
+
+    public static void register(ExprTranslator t)
+    {
+        translators.put(t.type(), t);
+    }
+
+    public static ExprTranslator getTranslator(Expr.EType type)
+    {
+        if (translators.containsKey(type))
+            return (translators.get(type));
+        return (null);
+    }
+
+    static
+    {
+
+    }
 }
